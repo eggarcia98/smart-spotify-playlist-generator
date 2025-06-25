4029996cdf8be3350205f89f1ac0653fe3016556
Erick Garcia <eggarcia9814@gmail.com>
2025-06-25 12:11:35 +1000

    Refactor server initialization to improve structure and use config for Spotify token retrieval

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,24 @@ import fastifyEnv from "@fastify/env";
 import fastifyEnvOptions from "./plugins/env";
 import { getSpotifyToken } from "./services/spotifyAuth";
 
-const fastifyInstance = Fastify({ logger: true });
-
-const server = async () => {
+async function buildApp() {
+    const fastifyInstance = Fastify({ logger: true });
 
     await fastifyInstance.register(fastifyEnv, fastifyEnvOptions);
-    const envs = fastifyInstance.getEnvs<EnvConfig>();
-    
-    fastifyInstance.get("/ping", async (request, reply) => {
-        return { pong: true };
-    });
+    const config = fastifyInstance.config;
+
+    fastifyInstance.get("/ping", async () => ({ pong: true }));
 
     fastifyInstance.get("/spotify-token", async (_, reply) => {
-        const token = await getSpotifyToken(envs);
+        const token = await getSpotifyToken(config);
         reply.send({ token });
     });
+
+    return fastifyInstance;
+}
+
+const server = async () => {
+    const fastifyInstance = await buildApp();
 
     try {
         await fastifyInstance.listen({
@@ -26,7 +29,9 @@ const server = async () => {
             host: "0.0.0.0",
         });
 
-        console.log(`Server is running at http://localhost:${process.env.PORT || 3000}`);
+        console.log(
+            `Server is running at http://localhost:${process.env.PORT || 3000}`
+        );
     } catch (err) {
         fastifyInstance.log.error(err);
         process.exit(1);
